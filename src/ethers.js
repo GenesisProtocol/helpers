edd0fb6c8e43e2e26ca713ebeb65765f40516b93
AdamGenesis <adam@genesis.com>
2022-03-07 11:31:48 -0300

    Added more ethers helpers

--- src/ethers.js
+++ src/ethers.js
@@ -3,6 +3,8 @@ const { getABI } = require('./abis')
 
 const getChain = async () => (await ethers.provider.getNetwork()).chainId
 
+const getBlock = () => ethers.provider.getBlock('latest')
+
 const getContract = async ({ name }) => new ethers.Contract(
 	await getAddress({
 		chain: await getChain(),
@@ -12,7 +14,17 @@ const getContract = async ({ name }) => new ethers.Contract(
 	(await ethers.getSigners())[0]
 )
 
+const send = async (tx, wait = 2) => {
+	tx = await tx
+
+	await tx.wait(wait)
+
+	return tx
+}
+
 module.exports = {
 	getChain,
 	getContract,
+	send,
+	getBlock,
 }
