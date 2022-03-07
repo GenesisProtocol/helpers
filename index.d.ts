82cbfbf6b86c797a30d40f5a9ab2e1f3e16b67f7
AdamGenesis <adam@genesis.com>
2022-03-07 19:44:44 -0300

    Added getChain to interface

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
 export function getAddress({ chain, name }: { chain?: number, name: string }): Promise<string>;
 
 export function getABI({ name }: { name: string }): Promise<string[]>;
+
+export function getChain(): Promise<number>;
