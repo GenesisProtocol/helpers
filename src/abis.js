3d7097dec8f61f0ced51a1f3980e53ea68276149
AdamGenesis <adam@genesis.com>
2022-03-07 12:10:52 -0300

    Bugfixes

--- src/abis.js
+++ src/abis.js
@@ -1,7 +1,6 @@
 const fs = require('fs').promises
 const path = require('path')
 const Case = require('case')
-const ethers = require('ethers')
 
 const getFilePath = name => path.join(__dirname, `../data/abis/${Case.constant(name)}.json`)
 
