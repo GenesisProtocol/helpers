1eea902a50699430e35c1b7e79ba278c74c653c3
AdamGenesis <adam@genesis.com>
2022-03-07 18:01:46 -0300

    Optional chain parameter on getAddress

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-export function getAddress({ chain, name }: { chain: number, name: string }): Promise<string>;
+export function getAddress({ chain, name }: { chain?: number, name: string }): Promise<string>;
 
 export function getABI({ name }: { name: string }): Promise<string[]>;
