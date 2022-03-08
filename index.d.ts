86e56337bb144041d78b657b90ff831ff0109c90
AdamGenesis <adam@genesis.com>
2022-03-07 22:18:29 -0300

    Added methods to interface

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,11 @@
+import {ethers} from "ethers";
+
 export function getAddress({ chain, name }: { chain?: number, name: string }): Promise<string>;
 
 export function getABI({ name }: { name: string }): Promise<string[]>;
 
 export function getChain(): Promise<number>;
+
+export function getContract({name}: {name: string}): Promise<ethers.Contract>
+
+export function send(tx: any): Promise<any>
