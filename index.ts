9c165ff51b6742d12defdffeb93418f2371254f7
Noah Niguiri <noah.genesis@protonmail.com>
2022-05-06 00:28:39 -0300

    update getAddress return type

--- index.ts
+++ index.ts
@@ -16,6 +16,7 @@ export const chains: ChainMap = chainsData
 
 export const addresses: AddressMap = addressesData
 
-export const getAddress = ({ chain, name }: { chain?: number, name: string }): string => addresses?.[chain || -1]?.[Case.constant(name)]
+export const getAddress = ({ chain, name }: { chain?: number, name: string }): string | undefined =>
+	addresses?.[chain || -1]?.[Case.constant(name)]
 
 export const getAbi = async ({ name }: { name: string }): Promise<string[]> => (await import(`./data/abis/${Case.constant(name)}.json`)).default
